1d595b962a2c731bbb94cd539d7defa11fd6dd86
Matthieu Ancellin <matthieu.ancellin@cmla.ens-cachan.fr>
2019-09-01 19:54:32 +0200

    Add script to compile .tikz file.

---
+++ show.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env bash
+
+TMP_DIR=$(mktemp -d)
+
+# HEADER
+cat <<EOF >> "$TMP_DIR/source.tex"
+\documentclass{standalone}
+\usepackage{tikz}
+\begin{document}
+    \begin{tikzpicture}[]
+EOF
+
+# BODY
+cat $1 >> "$TMP_DIR/source.tex" 
+
+# FOOTER
+cat <<EOF >> "$TMP_DIR/source.tex" 
+    \end{tikzpicture}
+\end{document}
+EOF
+
+# COMPILATION
+latexmk -aux-directory=$TMP_DIR -output-directory=$TMP_DIR "$TMP_DIR/source.tex"
+
+# DISPLAY
+zathura "$TMP_DIR/source.pdf"
+
